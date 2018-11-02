d6926db6450ac80ba9ac42760fa898d4bb3f3764
dimmxx <dimmxx@gmail.com>
2018-11-02 19:05:56 +0200

    02.11.18

--- src/sorttypes/GenerateArray.java
+++ src/sorttypes/GenerateArray.java
@@ -1,44 +1,44 @@
 package sorttypes;
+import java.util.Arrays;
 import java.util.Random;
 
 public class GenerateArray {
 
     private static char[] charBase = {' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'};
 
-
-    private static final int SIZE = 100;
+    private static final int SIZE = 1000;
     private static final int WORDLENGTH = 10;
 
     private static int[] intArray = new int[SIZE];
-    private static double[] doubleArray = new double[SIZE];
+    private static float[] floatArray = new float[SIZE];
     private static String[] stringArray = new String[SIZE];
 
     private static Random random = new Random();
 
-
-
-
-
     public static void main(String[] args) {
 
+// generate string array ======================================================================================
+
+        char[][] tempChar = new char[SIZE][WORDLENGTH];
+        for(int i = 0; i < SIZE; i++){
+            for(int k = 0; k < WORDLENGTH; k++){
+                tempChar[i][k] = charBase[random.nextInt(charBase.length)];
+            }
+        }
+        for (int i = 0; i < tempChar.length; i++){
+            System.out.println(Arrays.toString(tempChar[i]));
+        }
+        for (int i = 0; i < SIZE; i++){
+            stringArray[i] = new String(tempChar[i]);
+        }
+        System.out.println(Arrays.toString(stringArray));
 
 
-        char[] temp = new char[WORDLENGTH];
+// generate float array =======================================================================================
 
         for(int i = 0; i < SIZE; i++){
-
-
-
-            for(int k = 0; k < WORDLENGTH; k++){
-
-                stringArray[i][k] =;
-
-            }
-
+            
         }
-
-
-
 
 
     }
---
+++ src/sorttypes/temp.java
@@ -0,0 +1,25 @@
+package sorttypes;
+
+import java.util.Arrays;
+
+public class temp {
+
+
+    public static void main(String[] args) {
+
+        char[] array = {'f', 'o', 'o'};
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = 'g';
+            System.out.println(Arrays.toString(array));
+        }
+
+
+
+
+    }
+
+
+
+
+}
