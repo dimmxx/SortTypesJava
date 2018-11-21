dd06b0e26bede4a47e6a18d3b4894f7ba538f63a
dimmxx <dimmxx@gmail.com>
2018-11-21 18:42:13 +0200

    21.11.18

--- src/sorttypes/temp.java
+++ src/sorttypes/temp.java
@@ -1,28 +1,60 @@
 package sorttypes;
 
+import java.util.Arrays;
+
 public class temp {
+
+    static int[] array = {1, 8, 2, 7, 1, 2, 7};
 
     public static void main(String[] args) {
 
-        int[] array = {8, 2, 10, 7, 0, 1, 0, 2, 5, 7};
-
         int start = 0;
         int end = array.length - 1;
-        int pivot = (start + end) / 2
 
 
-        rellocateQ(start, end, pivot);
+
+        rellocateQ(start, end);
     }
 
-    public static void rellocateQ (int start, int end, int pivot){
+    public static void rellocateQ (int start, int end){
 
+        if(end > start + 1){
+            int pivot = (start + end) / 2;
+            for (int i = start; i < pivot; i++) {
+                if (array[i] > array[pivot]){
+                    pivot = move(i, pivot);
 
+                }
+
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
 
 
 
 
 
     }
+
+
+    public static int move (int ndx, int pivot){
+        int tempI = array[ndx];
+        //array[pivot] = array[ndx];
+        for (int i = ndx; i < pivot; i++) {
+            array[i] = array[i + 1];
+        }
+        array[pivot] = tempI;
+        pivot--;
+
+
+
+
+
+        return pivot;
+
+    }
+
 
 
 }
