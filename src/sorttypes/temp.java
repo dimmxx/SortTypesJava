1d185653571359cb95374928ffea1139554da8ed
dimmxx <dimmxx@gmail.com>
2018-11-27 08:05:51 +0200

    27.11.18

--- src/sorttypes/temp.java
+++ src/sorttypes/temp.java
@@ -8,10 +8,26 @@ public class temp {
 
     public static void main(String[] args) {
 
-
     }
 
-    public static 
+    public static int[] mergeSort (int[] array1, int[] array2){
+
+        int[] arrayRes = new int[array1.length + array2.length];
+        for (int i = 0; i < arrayRes.length; i++){
+            if (array1[i] <= array2[i]){
+                arrayRes[i] = array1[i];
+                
+            }
+        }
+
+
+
+
+
+
+
+        return arrayRes;
+    }
 
 
 }
